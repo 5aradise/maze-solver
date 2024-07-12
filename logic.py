238b7e1e842f1ae4e92f59fb66dd178b48ec14cc
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-07-12 16:59:57 +0300

    add maze generator

--- logic.py
+++ logic.py
@@ -2,6 +2,12 @@ from graphics import *
 from window import Window
 import theme
 import time
+import random
+
+
+class Drawable:
+    def draw():
+        pass
 
 
 class Maze:
@@ -9,15 +15,18 @@ class Maze:
                  pos: Vec2,
                  num_cols: int,
                  num_rows: int,
-                 cell_size: float) -> None:
+                 cell_size: float,
+                 seed=None) -> None:
         self.pos = pos
         self.num_cols = num_cols
         self.num_rows = num_rows
         self.cell_size = cell_size
-        self.to_draws = []
+        self.__to_draws = list[Drawable]()
         self._cells = list[list[Cell]]()
+        random.seed(seed)
         self._create_cells()
         self._create_entrance_and_exit()
+        self._create_maze()
 
     def _create_cells(self):
         curr_pos = self.pos.copy()
@@ -25,7 +34,7 @@ class Maze:
             cell_row = list[Cell]()
             for _ in range(self.num_cols):
                 cell = Cell(curr_pos.copy(), self.cell_size)
-                self.to_draws.append(cell.copy())
+                self.__to_draws.append(cell.copy())
                 cell_row.append(cell)
                 curr_pos += Vec2(self.cell_size, 0)
             self._cells.append(cell_row)
@@ -34,15 +43,87 @@ class Maze:
 
     def _create_entrance_and_exit(self):
         self._cells[0][0].l_wall = False
-        self.to_draws.append(self._cells[0][0].copy())
+        self.__to_draws.append(self._cells[0][0].copy())
         self._cells[-1][-1].r_wall = False
-        self.to_draws.append(self._cells[-1][-1].copy())
+        self.__to_draws.append(self._cells[-1][-1].copy())
+
+    def _create_maze(self):
+        self._cells[0][0].visited = True
+        self._create_maze_r(0, 0)
+        self._reset_visiting()
+
+    def _create_maze_r(self, start_i: int, start_j: int):
+        start_cell = self._cells[start_i][start_j]
+        visited_neigh = 0
+        while visited_neigh != 4:
+            visited_neigh = 0
+
+            if (0 <= start_j-1
+                    and not self._cells[start_i][start_j-1].visited):
+                if random.randint(0, 2) == 0:
+                    visited_neigh += 1
+                    l_cell = self._cells[start_i][start_j-1]
+                    l_cell.visited = True
+                    start_cell.l_wall = False
+                    l_cell.r_wall = False
+                    self.__to_draws.append(start_cell)
+                    self.__to_draws.append(l_cell)
+                    self._create_maze_r(start_i, start_j-1)
+            else:
+                visited_neigh += 1
+
+            if (0 <= start_i-1
+                    and not self._cells[start_i-1][start_j].visited):
+                if random.randint(0, 2) == 0:
+                    visited_neigh += 1
+                    t_cell = self._cells[start_i-1][start_j]
+                    t_cell.visited = True
+                    start_cell.t_wall = False
+                    t_cell.b_wall = False
+                    self.__to_draws.append(start_cell)
+                    self.__to_draws.append(t_cell)
+                    self._create_maze_r(start_i-1, start_j)
+            else:
+                visited_neigh += 1
+
+            if (start_j+1 <= self.num_cols-1
+                    and not self._cells[start_i][start_j+1].visited):
+                if random.randint(0, 2) == 0:
+                    visited_neigh += 1
+                    r_cell = self._cells[start_i][start_j+1]
+                    r_cell.visited = True
+                    start_cell.r_wall = False
+                    r_cell.l_wall = False
+                    self.__to_draws.append(start_cell)
+                    self.__to_draws.append(r_cell)
+                    self._create_maze_r(start_i, start_j+1)
+            else:
+                visited_neigh += 1
+
+            if (start_i+1 <= self.num_rows-1
+                    and not self._cells[start_i+1][start_j].visited):
+                if random.randint(0, 2) == 0:
+                    visited_neigh += 1
+                    b_cell = self._cells[start_i+1][start_j]
+                    b_cell.visited = True
+                    start_cell.b_wall = False
+                    b_cell.t_wall = False
+                    self.__to_draws.append(start_cell)
+                    self.__to_draws.append(b_cell)
+                    self._create_maze_r(start_i+1, start_j)
+            else:
+                visited_neigh += 1
+
+    def _reset_visiting(self):
+        for row in self._cells:
+            for cell in row:
+                cell.visited = False
 
     def draw(self, win: Window):
-        for to_draw in self.to_draws:
+        for to_draw in self.__to_draws:
             to_draw.draw(win)
             win.redraw()
-            time.sleep(0.02)
+            time.sleep(0.015)
 
 
 class Cell:
@@ -51,11 +132,13 @@ class Cell:
                  has_left_wall=True,
                  has_right_wall=True,
                  has_top_wall=True,
-                 has_bottom_wall=True) -> None:
+                 has_bottom_wall=True,
+                 visited=False) -> None:
         self.l_wall = has_left_wall
         self.r_wall = has_right_wall
         self.t_wall = has_top_wall
         self.b_wall = has_bottom_wall
+        self.visited = visited
         self._pos = pos
         self._size = size
         self.__body = [
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from graphics import *
 
 
 def main():
-    win_width = 1600
-    win_height = 1200
+    win_width = 800
+    win_height = 600
     win = Window(win_width, win_height)
 
-    maze = Maze(Vec2(win_width/2 - 8 * 70, win_height/2 - 6 * 70), 16, 12, 70)
+    maze = Maze(Vec2(win_width/2 - 8 * 40, win_height/2 - 6 * 40), 16, 12, 40)
     maze.draw(win)
 
     win.wait_for_close()
